6022da4038b8c76efa1dc52a93135b028c6a7baa
indrachyd <indraredtrlv@gmail.com>
2020-06-24 10:27:06 +0700

    Initial Commit

---
+++ MandiriNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MandiriNews
+//
+//  Created by Indra Cahyadi on 24/06/20.
+//  Copyright © 2020 Indra Cahyadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
